96bfdca2fcf1cd9c8dcedee49c58a6591f0074f7
berkforbes <berkforbes@yahoo.com>
2022-01-23 21:19:46 -0700

    beginning work on user interface

--- server.js
+++ server.js
@@ -17,21 +17,21 @@ const connection = mysql.createConnection({
     database: 'employees'
   });
  
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
- 
-// test function
-app.get('/', (req, res) => {
-   res.json({
-     message: 'Hello World'
-   });
- });
- 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-   res.status(404).end();
- });
+connection.connect(err => {
+    if (err) throw err;
+    welcomeMessage();
+})
+
+welcomeMessage = () => {
+    console.log ('------------------------')
+    console.log ('|                      |')
+    console.log ('|                      |')
+    console.log ('|   EMPLOYEE TRACKER   |')
+    console.log ('|                      |')
+    console.log ('|                      |')
+    console.log ('------------------------')
+}
+
  
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
