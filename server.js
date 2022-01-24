5b7547cd5bbddafd216cf7aecdf6562dee529343
berkforbes <berkforbes@yahoo.com>
2022-01-23 21:34:07 -0700

    "what would  you like to do" q's and a's

--- server.js
+++ server.js
@@ -30,6 +30,55 @@ welcomeMessage = () => {
     console.log ('|                      |')
     console.log ('|                      |')
     console.log ('------------------------')
+    promptUser();
+};
+
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 
+                      'View all roles', 
+                      'View all employees', 
+                      'Add a department', 
+                      'Add a role', 
+                      'Add an employee', 
+                      'Update an employee role']
+        }
+    ])
+        .then((answers) => {
+            const { choices } = answers;
+
+            if (choices === 'View all departments') {
+                showDepartments();
+            }
+
+            if (choices === 'View all roles') {
+                showDepartments();
+            }
+
+            if (choices === 'View all employees') {
+                showDepartments();
+            }
+
+            if (choices === 'Add a department') {
+                showDepartments();
+            }
+
+            if (choices === 'Add a role') {
+                showDepartments();
+            }
+
+            if (choices === 'Add an employee') {
+                showDepartments();
+            }
+
+            if (choices === 'Update an employee role') {
+                showDepartments();
+            }
+        })
 }
 
  
